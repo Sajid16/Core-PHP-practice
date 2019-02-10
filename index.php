cb0cb1a22e4c9e6ecf0814657ce3350745cabd45
sajid16 <sajid.mahboob16@gmail.com>
2019-02-11 04:09:15 +0600

    Update index.php

--- index.php
+++ index.php
@@ -86,6 +86,50 @@
 	// $param = "javasccript";
 	$param = $sw_var3;
 	switch_case_function($param);
+	echo "<br>";
+
+	// for loop practice
+
+	$number = 1;
+	while($number<11){
+
+		echo "The number is: ".$number;
+		echo "<br>";
+		$number++;
+
+	}
+
+	echo "<br>";
+
+	// do while loop
+
+	$test = 17;
+
+	do{
+		echo "The number is:".$test." that we have got using do while loop";
+		$test++;
+	}while ($test<10);
+
+	echo "<br>";
+
+	// for loop practice
+
+	$sum = 0;
+
+	for($i=1; $i<=10; $i++){
+
+		$result = addition($i);
+	}	
+
+	echo "The summation of the above loop range numbers are: ".$result;
+
+	function addition($sum,$i){
+
+		$sum = $sum + $i;
+		return $sum;
+		echo $sum."<br>";
+	}
+
 	?>
 </body>
 </html>
